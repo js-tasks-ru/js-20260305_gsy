69ac74c6d90ddad023d3087c2f2859d192425162
GoncharSY <gonchar.sy.dev@gmail.com>
2026-03-13 00:53:10 +0300

    Решил первую задачу (функция-геттер)

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const nodes = path.split('.');
 
+  return function(tree) {
+    const checkIsObj = (val) => tree instanceof Object;
+    const checkHasKey = (key) => Object.hasOwn(tree, key);
+
+    for (const node of nodes) {
+      if (!checkIsObj(tree) || !checkHasKey(node)) {return;}
+
+      tree = tree[node];
+    }
+
+    return tree;
+  };
 }
