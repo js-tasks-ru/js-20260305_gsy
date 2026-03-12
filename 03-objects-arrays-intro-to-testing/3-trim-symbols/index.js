0985c4cdba63993423e1fa653a039efc18bfe7a2
GoncharSY <gonchar.sy.dev@gmail.com>
2026-03-13 02:11:17 +0300

    Решил третью задачу (одинаковые символы в строке)

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,23 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let str = '';
+  let sym = '';
+  let cnt = 0;
 
+  if (size === undefined) {return string;}
+
+  for (const s of string) {
+    if (s !== sym) {
+      str += sym.repeat(cnt);
+      sym = s;
+      cnt = 0;
+    }
+
+    if (cnt < size) {
+      cnt += 1;
+    }
+  }
+
+  return str + sym.repeat(cnt);
 }
